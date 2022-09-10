850610e9cd2e668f9a6b0b3a811580ed3c6470f8
Nguyễn Trọng Hậu <akitosama12546@gmail.com>
2022-09-10 18:48:18 +0700

    cst fe 4 commit update environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'https://haunt-fe-castone-app.herokuapp.com', // the running FLASK api server url
   auth0: {
     url: 'dev-a5eeh4cq.us', // the auth0 domain prefix
     audience: 'CapStone-app', // the audience set for the auth0 app
     clientId: 'm7nDjkojNjC0zumBAGZG9tkBBLXwOX5F', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
+    callbackURL: 'https://haunt-fe-castone-app.herokuapp.com', // the base url of the running ionic application.
   }
 };
 
